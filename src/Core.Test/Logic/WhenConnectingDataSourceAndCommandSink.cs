1361d1390d575b4843829dbb26508fb106f1a4a0
Tim Slechten <tim.slechten@dematic.com>
2019-11-19 12:39:18 +0100

    implement Connector

---
+++ src/Core.Test/Logic/WhenConnectingDataSourceAndCommandSink.cs
@@ -0,0 +1,32 @@
+﻿using FakeItEasy;
+using HC.Core.DataTypes;
+using HC.Core.Design;
+using HC.Core.Logic;
+using Xunit;
+
+namespace HC.Core.Test.Logic
+{
+  public class WhenConnectingDataSourceAndCommandSink
+  {
+    private readonly IDataSource _dataSource;
+
+    private readonly ICommandSink _commandSink;
+
+    private readonly Connector _connector;
+
+    public WhenConnectingDataSourceAndCommandSink()
+    {
+      _dataSource = A.Fake<IDataSource>();
+      _commandSink = A.Fake<ICommandSink>();
+      _connector = new Connector(_dataSource, _commandSink);
+    }
+
+    [Fact]
+    public void WithNewData_CommandShouldBeProcessed()
+    {
+      _dataSource.DataUpdated += Raise.FreeForm.With(_dataSource, new IntegerData(42));
+
+      A.CallTo(() => _commandSink.ProcessCommand(new IntegerData(42))).MustHaveHappened();
+    }
+  }
+}
---
+++ src/Core/Logic/Connector.cs
@@ -0,0 +1,29 @@
+﻿using HC.Core.Design;
+
+namespace HC.Core.Logic
+{
+  public class Connector
+  {
+    private readonly IDataSource _dataSource;
+
+    private readonly ICommandSink _commandSink;
+
+    public Connector(IDataSource dataSource, ICommandSink command)
+    {
+      _dataSource = dataSource;
+      _commandSink = command;
+
+      _dataSource.DataUpdated += OnDataUpdated;
+    }
+
+    private void OnDataUpdated(IDataSource dataSource, IData newData)
+    {
+      SendCommand(newData);
+    }
+
+    private void SendCommand(IData data)
+    {
+      _commandSink.ProcessCommand(data);
+    }
+  }
+}
